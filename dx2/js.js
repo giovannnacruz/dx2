a0b3544a8c3c1f690e1f9255b6b29b49a5caa528
giovanna cristina <133894598+giovannnacruz@users.noreply.github.com>
2023-10-04 15:46:09 -0300

    Delete dx2 directory

--- dx2/js.js
+++
@@ -1,5 +0,0 @@
-function nomes(){
-        if(document.querySelector('#pwd').value == "integrantes"){
-    document.querySelector("h1").innerHTML= "Davi Almeida<br>Giovanna Cristina<br>Moisés Camargo<br>Pedro Henrique"
-        }
-}
