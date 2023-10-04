ae209c4e6853f97afb69e5823f560fc97632a85d
giovanna cristina <133894598+giovannnacruz@users.noreply.github.com>
2023-10-04 15:46:40 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+function nomes(){
+        if(document.querySelector('#pwd').value == "integrantes"){
+    document.querySelector("h1").innerHTML= "Davi Almeida<br>Giovanna Cristina<br>Moisés Camargo<br>Pedro Henrique"
+        }
+}
